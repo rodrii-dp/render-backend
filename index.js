a5b3870bb0b1193668760488fdd6e8e352b5125b
Rodrigo <rrddpp1@gmail.com>
2024-01-16 18:37:33 +0100

    add a log

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
   console.log('Body:  ', request.body)
+  console.log(request);
   console.log('---')
   next()
 }
